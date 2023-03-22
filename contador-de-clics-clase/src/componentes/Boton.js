676ec4da675ee4c68953780f32b91af40c99106e
Cynthia Gaffet <codificar76@gmail.com>
2023-03-22 13:03:23 -0300

    proyecto-freecodecamp

---
+++ contador-de-clics-clase/src/componentes/Boton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../hojas-de-estilo/Boton.css';
+
+class Boton extends React.Component {
+    render() {
+        return(
+            <button
+            className={this.props.esBotonDeClic ? 'boton-clic': 'boton-reiniciar'}
+                oncClick={this.props.manejarClic}>
+            {this.props.texto}
+            </button>
+        );
+    }
+}
+export default Boton;
